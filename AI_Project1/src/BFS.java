add3711c43b7be783ebb7c60b421afe99fd33086
Mohammad Mahdi Samiei <mohmahsamiei@gmail.com>
2016-10-28 01:25:00 +0330

    Correction of Implementation of Queue,
    using LinkedList instead Queue
    add Max Memory Nodes

--- AI_Project1/src/BFS.java
+++ AI_Project1/src/BFS.java
@@ -1,6 +1,10 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import javax.swing.plaf.nimbus.State;
 
-import sun.misc.Queue;
 
 public class BFS<S, A> {
 	Problem<S, A> problem;
@@ -8,50 +12,49 @@ public class BFS<S, A> {
 	public BFS(Problem<S, A> problem) {
 
 		this.problem = problem;
-		search();
+		treeSearch();
 	}
 
-	public void search() {
+	public void treeSearch() {
 		int observedNodes = 0;
 		int expandedNodes = 0;
 		int maxQueueSize = 0;
 		S first = problem.initiallState();
 		observedNodes++;
-		Queue<Node<S>> queue = new Queue<Node<S>>();
-		queue.enqueue(new Node<S>(first));
+		Queue<Node<S>> queue = new LinkedList<Node<S>>();
+		//Queue<Node<S>> queue = new Queue<Node<S>>();
+		queue.add(new Node<S>(first));
 		while (true) {
 			if (queue.isEmpty()) {
 				System.out.println("error");
 				System.out.println("I can't find Soluton");
 			} else {
 				Node<S> current;
-				try {
-					current = queue.dequeue();
-					expandedNodes++;
-					for (A a : problem.actions(current.state)) {
-						Node<S> newNode = new Node<S>(current, problem.result(
-								current.state, a), problem.cost(current.state,
-								problem.result(current.state, a), a));
-						observedNodes++;
-						queue.enqueue(newNode);
-						if (problem.goal(newNode.state) == true) {
-							System.out.println("I have found Solution");
-							System.out.println("I have seen " + observedNodes
-									+ " nodes");
-							System.out.println("I have expanded "
-									+ expandedNodes + " nodes");
-							System.out.println("the path is:\n\n");
-							printPath(newNode);
-							System.out.println("this cost of path is : "
-									+ newNode.cost);
-							int x='x';//Size
-							Thread.sleep(10000);
-						}
+				current = queue.poll();
+				expandedNodes++;
+				for (A a : problem.actions(current.state)) {
+					Node<S> newNode = new Node<S>(current, problem.result(
+							current.state, a), problem.cost(current.state,
+							problem.result(current.state, a), a));
+					observedNodes++;
+					queue.add(newNode);
+					if (problem.goal(newNode.state) == true) {
+						System.out.println("I have found Solution");
+						System.out.println("I have seen " + observedNodes
+								+ " nodes");
+						System.out.println("I have expanded "
+								+ expandedNodes + " nodes");
+						System.out.println("the path is:\n");
+						printPath(newNode);
+						System.out.println("\nthis cost of path is : "
+								+ newNode.cost);
+						System.out.println("the Max Memory usage is "+maxQueueSize+" nodes\n");
+						return;
 					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
 				}
 			}
+			if(maxQueueSize<queue.size())
+				maxQueueSize=queue.size();
 		}
 	}
 
